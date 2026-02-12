992de768e473ec18103902188f7c020c54f6793c
Jamie <nicholls_jamie@outlook.com>
2026-02-12 20:45:58 +0000

    feat: Initialize Electron project with Electron Forge configuration and a basic "Hello World" application structure.

---
+++ application.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron/main');
+const path = require('node:path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        }
+    });
+
+    win.loadFile('public/index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
