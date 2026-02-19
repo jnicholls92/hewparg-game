959dd9dfed4cc2cf4a673f36a5d39c23153f6468
Jamie <nicholls_jamie@outlook.com>
2026-02-19 19:38:02 +0000

    feat: Implement auto-update functionality, add a development script, and update the package version.

--- application.js
+++ application.js
@@ -1,7 +1,29 @@
-const { app, BrowserWindow } = require('electron/main');
+const { app, BrowserWindow, autoUpdater } = require('electron/main');
 const path = require('path');
 
+const server = 'https://update.electronjs.org'
+const url = `${server}/updates/${process.platform}/${app.getVersion()}`
+autoUpdater.setFeedURL({ url })
 
+autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...');
+});
+
+autoUpdater.on('update-available', () => {
+    console.log('Update available!');
+});
+
+autoUpdater.on('update-not-available', () => {
+    console.log('Update not available!');
+});
+
+autoUpdater.on('update-downloaded', () => {
+    console.log('Update downloaded!');
+});
+
+autoUpdater.on('error', (error) => {
+    console.log('Update error:', error);
+});
 
 function createWindow() {
     const win = new BrowserWindow({
